0b0f17aa66ed0527fc94dd4288dc524bd457a19c
SilentProton <silentproton.25@gmail.com>
2025-07-26 17:09:20 +0530

    chore: remove unused handleClick function in MagicButton

--- src/components/ui/MagicButton.tsx
+++ src/components/ui/MagicButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const MagicButton = (
-    {title, icon, position, handleClick, otherClasses}:{title: string, icon: React.ReactNode, position: string, handleClick?: () => void, otherClasses?: string}
+    {title, icon, position,  otherClasses}:{title: string, icon: React.ReactNode, position: string,  otherClasses?: string}
     ) => {
     return (
         <button className="relative inline-flex h-12 w-full overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 md:mt-10 ">
